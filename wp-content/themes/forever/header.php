50de58681b44682b0944f399d57870ee82014c1d
franck <colfr.76@gmail.com>
2017-08-27 19:06:36 +0200

    Add header title meta

--- wp-content/themes/forever/header.php
+++ wp-content/themes/forever/header.php
@@ -18,7 +18,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Studio K</title>
+<title><?php  bloginfo('name'); ?><?php wp_title();   ?></title>
 <?php wp_head(); ?>
 </head>
 <body>
