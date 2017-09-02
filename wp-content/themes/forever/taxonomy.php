4e3ec4c5493a69610331221fdb054bc9c6e91bb7
franck <colfr.76@gmail.com>
2017-09-02 14:11:37 +0200

    ok edit css for subtitles [connect #5]

--- wp-content/themes/forever/taxonomy.php
+++ wp-content/themes/forever/taxonomy.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
-<div class="container">
+<div class="container-fluid">
 		<div class="row">
-		     <div class="col-lg-12 col-md-12 col-xs-12 content_top-marge">
+		     <div class="col-lg-10 col-md-12 col-xs-12 col-lg-offset-1 content_top-marge">
 		     	<?php
 		if (have_posts()): /*debut de la boucle */ ?>
 					<?php
     while (have_posts()):
         the_post(); ?>
-						<div class="col-lg-3 col-md-3 col-xs-6 offset-bottom">
+						<div class="col-lg-3 col-md-3  col-sm-4  offset-bottom">
 							<a href="<?php the_permalink(); ?>" class="item_link-hover" >
 				    		    <figure>
 				    		        <?php the_post_thumbnail('thumbnail'); ?>
